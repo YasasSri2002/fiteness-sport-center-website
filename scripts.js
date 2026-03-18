efb7c1dd17ecc6f7707726c7e945238d67634e6e
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-18 09:34:03 +0530

    custom alert box

--- scripts.js
+++ scripts.js
@@ -16,3 +16,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
+function showAlert(title, message, type = "success") {
+  const overlay = document.getElementById("custom-alert");
+  const icon = document.getElementById("alert-icon");
+
+  document.getElementById("alert-title").innerText = title;
+  document.getElementById("alert-message").innerText = message;
+
+  let svg = "";
+
+  if (type === "success") {
+    svg = `
+      <div class="flex justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" class="bi bi-check-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+        </svg>
+      </div>
+    `;
+  }
+
+  if (type === "error") {
+    svg = `
+      <div class="flex justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="red" class="bi bi-x-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+        </svg>
+      </div>
+    `;
+  }
+
+  if (type === "warning") {
+    svg = `
+      <div class="flex justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="orange" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+        </svg>
+      </div>
+    `;
+  }
+
+  icon.innerHTML = svg;
+
+  overlay.classList.add("active");
+
+  // ⏱ auto close
+  setTimeout(() => {
+    overlay.classList.remove("active");
+  }, 2500);
+}
+
+function closeAlert() {
+  const overlay = document.getElementById("custom-alert");
+
+  overlay.classList.remove("active");
+}
+
+function submitContactForm(event){
+    event.preventDefault();
+    showAlert("Success!","Message has been send!", "success");
+}
+
